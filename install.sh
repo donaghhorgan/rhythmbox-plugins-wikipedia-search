3628f505a38b485a19bb7b6f42b7c81eb2406a5f
Donagh Horgan <donagh.horgan@gmail.com>
2012-10-13 01:58:32 +0100

    Changed title of plugin to WikipediaSearch.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 SCRIPT_NAME=`basename "$0"`
 SCRIPT_PATH=${0%`basename "$0"`}
-OLD_PATH="/home/${USER}/.local/share/rhythmbox/plugins/wikipedia/"
-PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/Wikipedia/"
+OLD_PATH_1="/home/${USER}/.local/share/rhythmbox/plugins/wikipedia/"
+OLD_PATH_2="/home/${USER}/.local/share/rhythmbox/plugins/Wikipedia/"
+PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/WikipediaSearch/"
 
-rm -rf $OLD_PATH
+rm -rf $OLD_PATH_1
+rm -rf $OLD_PATH_2
 rm -rf $PLUGIN_PATH
 
 mkdir -p $PLUGIN_PATH
